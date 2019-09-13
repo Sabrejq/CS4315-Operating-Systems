0224798dd906d55c757941b6ed0c60c6698e9ac6
Julio Quintero <sabrejq77@gmail.com>
2019-09-13 14:02:47 -0500

    Add files via upload

---
+++ CS_3304_Data_and_Information_Structures /4. Binary Search Tree/Code/Pr20-5.cpp
@@ -0,0 +1,30 @@
+// This program demonstrates the BinaryTree class template.
+// It builds a binary tree with 5 nodes.
+#include <iostream>
+#include "BinaryTree.h"
+#include<string>
+using namespace std;
+
+const int NUM_NODES = 5;
+
+int main()
+{
+   string name;
+   
+   // Create the binary tree.
+   BinaryTree<string> tree;
+
+   // Insert some names.
+   for (int count = 0; count < NUM_NODES; count++)
+   {
+       cout << "Enter an name: ";
+       getline(cin, name);
+       tree.insertNode(name);
+   }
+    
+   // Display the values.
+   cout << "\nHere are the values in the tree:\n";
+   tree.displayInOrder();
+   system("pause");
+   return 0;
+}
